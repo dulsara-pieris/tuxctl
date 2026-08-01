b4563f34d1a310d3ecc9ba6a0d81531552166c32
dulsara-pieris <dulsara.synapsnex@gmail.com>
2026-08-01 22:09:48 +0530

    bruh typo fixed

--- tuxctl/main.py
+++ tuxctl/main.py
@@ -5,7 +5,7 @@
 #network_problems = diagnose_network(network)
 
 from packages.main import find_package_file
-from package.main import install_package
+from packages.main import install_package
 
 import sys
 from utils.colours import *
