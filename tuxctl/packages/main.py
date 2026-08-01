95e6111d18f34538c511142281deccc061fcc653
dulsara-pieris <dulsara.synapsnex@gmail.com>
2026-08-01 20:36:10 +0530

    package thing url in base url fixed

--- tuxctl/packages/main.py
+++ tuxctl/packages/main.py
@@ -1,9 +1,10 @@
 import requests
 
-base_url = "https://raw.githubusercontent.com/dulsara-pieris/tuxctl/main/package_scripts/"
+base_url = "https://raw.githubusercontent.com/dulsara-pieris/tuxctl/package_scripts/"
 
 def find_package_file(target):
     
     package_url = f"{base_url}{target}.sh"
     response = requests.get(package_url)
+    print(package_url)
     print(response.status_code)
