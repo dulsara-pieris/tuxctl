1dbb90100f088472d6357764b00413c6f4c47090
dulsara-pieris <dulsara.synapsnex@gmail.com>
2026-08-02 21:32:04 +0530

    made pip package and now interacting part with installing bash file

--- tuxctl/main.py
+++ tuxctl/main.py
@@ -4,37 +4,43 @@
 #network = check_network_route()
 #network_problems = diagnose_network(network)
 
-from packages.main import find_package_file
-from packages.main import install_package
+from tuxctl.packages.main import find_package_file
+from tuxctl.packages.main import install_package
 
 import sys
-from utils.colours import *
+from tuxctl.utils.colours import *
 
-if len(sys.argv) < 2:
-    print("You might need help")
-    print("""
-    tuxctl - trouble shooting assistant
-    Usage:
-        tuxctl <command> <target>
-          """)
-    exit()
 
-action = sys.argv[1]
-
-if action in ["install", "remove"]:
-    if len(sys.argv) < 3:
-        print(f"{YELLOW}✗ Please Provide a package name{RESET}")
+def main():
+    if len(sys.argv) < 2:
+        print("You might need help")
+        print("""
+        tuxctl - trouble shooting assistant
+        Usage:
+            tuxctl <command> <target>
+                tuxctl install <package name>
+            """)
         exit()
-    else:
-        target = sys.argv[2]
-        if action == "install":
-            find_package_file(target)
-            response = find_package_file(target)
 
-            if response.status_code == 200:
-                print(f"{GREEN}✓{RESET} Downloaded {target} Package")
-                install_package(target)
-            elif response.status_code == 404:
-                print(f"{RED}✗ Package {target} not found{RESET}")
-            else:
-                print(f"{RED}✗ Server returned {response.status_code}{RESET}")
+    action = sys.argv[1]
+
+    if action in ["install", "remove"]:
+        if len(sys.argv) < 3:
+            print(f"{YELLOW}✗ Please Provide a package name{RESET}")
+            exit()
+        else:
+            target = sys.argv[2]
+            if action == "install":
+                find_package_file(target)
+                response = find_package_file(target)
+
+                if response.status_code == 200:
+                    print(f"{GREEN}✓{RESET} Downloaded {target} Package")
+                    install_package(target, response)
+                elif response.status_code == 404:
+                    print(f"{RED}✗ Package {target} not found{RESET}")
+                else:
+                    print(f"{RED}✗ Server returned {response.status_code}{RESET}")
+
+if __name__ == "__main__":
+    main()
--- tuxctl/packages/main.py
+++ tuxctl/packages/main.py
@@ -1,25 +1,48 @@
 import requests
 import os
 import subprocess
-from utils.colours import *
+import shutil
+
+from tuxctl.utils.colours import *
 
 base_url = "https://raw.githubusercontent.com/dulsara-pieris/tuxctl/Master/package_scripts/"
 tmp_package_dir = "/tmp/tuxctl/"
 
 def find_package_file(target):
-    
     package_url = f"{base_url}{target}.sh"
-    
+
     try:
         response = requests.get(package_url, timeout=20)
         return response
+    
+    #So don't get hours trying to recive package
     except requests.exceptions.Timeout:
         print(f"{RED}✗ Download timeout{RESET}")
         exit()
 
-def install_package(target):
+def install_package(target, response):
     os.makedirs(tmp_package_dir, exist_ok=True)
     package_file_path = f"{tmp_package_dir}{target}.sh"
 
+    #Check bash is there to run shell file
+    if shutil.which("bash") is None:
+        print(f"{RED}✗ Bash is a required package{RESET}")
+        exit(1)
+    
+    #Writing to package shell file in tmp folder
+    with open(package_file_path, "w") as file:
+        file.write(response.text)
+
+    print(f"{GREEN}✓{RESET} Download sucessfull")
+    
     os.chmod(package_file_path, 0o755)
-    subprocess.run([package_file_path])
+    
+    process = subprocess.Popen(
+        ["bash", package_file_path],
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+        text = True
+    )
+
+    for line in process.stdout:
+        print(line.strip())
