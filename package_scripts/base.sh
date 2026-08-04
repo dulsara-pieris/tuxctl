64e4b925d7cd95762fa260c01419346427e3ede4
dulsara-pieris <dulsara.synapsnex@gmail.com>
2026-08-04 13:17:38 +0530

    URL fixed 2nd try

--- package_scripts/base.sh
+++ package_scripts/base.sh
@@ -2,5 +2,5 @@
 #Please use this file as base to all package files
 
 echo "Download"
-aria2c -x8 -s8 -d /tmp/tuxctl https://proof.ovh.net/files/1Mb.dat
+aria2c -x8 -s8 -d /tmp/tuxctl https://geo.mirror.pkgbuild.com/core/os/x86_64/bash-5.3.3-1-x86_64.pkg.tar.zst
 #Please use higher values or lower depending on download size
