3e30a318eb95744ff5c9551f67fdce3112615270
dulsara-pieris <dulsara.synapsnex@gmail.com>
2026-08-04 13:20:27 +0530

    URL fixed 32nd try

--- package_scripts/base.sh
+++ package_scripts/base.sh
@@ -2,5 +2,5 @@
 #Please use this file as base to all package files
 
 echo "Download"
-aria2c -x8 -s8 -d /tmp/tuxctl https://geo.mirror.pkgbuild.com/core/os/x86_64/bash-5.3.3-1-x86_64.pkg.tar.zst
+aria2c -x8 -s8 -d /tmp/tuxctl https://ipv4.download.thinkbroadband.com/5MB.zip
 #Please use higher values or lower depending on download size
